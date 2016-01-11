e34d59579090ad8b0f5c68c3f4673d3ecf2d5095
Josh Rogan <jjjr1122@gmail.com>
2016-01-11 01:36:28 -0500

    Create players and stats framwork

---
+++ src/players/player.js
@@ -0,0 +1,50 @@
+/**
+ * Primary file for players 
+ */
+
+
+Name, Team, Position, Height, Weight, Age, 
+
+/**
+ * Get the stats for the player
+ * @return {Set} All of the stat objects for this player
+ */
+function getStats(){
+
+}
+
+/**
+ * Get the current contract for this player
+ * @return {Contract} [description]
+ */
+function getContract(){
+
+}
+
+
+/**
+ * Precict this players future disregrading any 
+ * impact of his teammates. 
+ * @return {[type]} [description]
+ */
+function predictFuture(){
+
+}
+
+/**
+ * Predict the future of this player considering his 
+ * teammates future. 
+ * @return {[type]} [description]
+ */
+function predictFutureWithTeam(){
+
+}
+
+/**
+ * Verify if the player ID is correct 
+ * @param  {String} potential_id [description]
+ * @return {boolean}              [description]
+ */
+static function verifyPlayer(potential_id){
+
+}
---
+++ src/players/stat.js
@@ -0,0 +1,7 @@
+// Base object for all stats
+Stat Name
+Stat Influence 
+
+
+//How many players of this position have the stat
+function frequency(){}
