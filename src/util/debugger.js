597b2493c54d5765b3611d828c589909cbea79b0
Josh Rogan <jjjr1122@gmail.com>
2016-02-07 21:28:20 -0500

    move code to debugger

--- src/util/debugger.js
+++ src/util/debugger.js
@@ -8,5 +8,7 @@ const DEFAULT_DEBUG = 0;
  * @return {[type]}            [description]
  */
 function debugMessage(debugLevel = DEFAULT_DEBUG){
-
+    //Logging with color examples
+    console.log('\x1b[36mHello from the log'); 
+    console.log('\x1b[36m', 'sometext' ,'\x1b[0m');
 }
