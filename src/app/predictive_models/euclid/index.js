97a98c2a9749bb087add7b320caeae979d7e6599
Josh Rogan <jjjr1122@gmail.com>
2016-05-27 00:25:33 -0400

    Working on euclid rewrite

--- src/app/predictive_models/euclid/index.js
+++ src/app/predictive_models/euclid/index.js
@@ -2,18 +2,20 @@
 import { Vector } from './../vectors';
 import { EuclidPair } from './../datastructures/EuclidPair';
 
+const players = []; // Array of Player objects
+const statArgs = []; // Array of the stat arguments (ex: ['standardBatting', 2015, 'H'])
 
-export class Euclid {
-
-  constructor(vectors) {
-    this.vectors = vectors;
+/**
+ * Create the same vector in order for every player
+ * @param  {Player} player
+ * @return {Vector|boolean}
+ */
+function createVector(player) {
+  const hits = player.getStat('standardBatting', 2015, 'H');
+  if (hits) {
+    return new Vector([hits]);
   }
-
-  add(vector) {
-    this.vectors.push(vector);
-  }
-
-
+  return false;
 }
 
 
